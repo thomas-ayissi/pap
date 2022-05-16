7fbf28a7ef8aa11fd0c87a02bbb487dab1b051fd
Thomas Ayissi <thomas8sea@gmail.com>
2022-05-16 16:11:40 +0200

    tpTriABulles.py
    mon essai de transcription de l'algorithme en code python

---
+++ tpTriABulles.py
@@ -0,0 +1,14 @@
+"""Tri a bulles"""
+def triABulles(l: list)-> list:
+
+    assert type(l)== list and len(l)>=0, "la liste doit contenir au moins un élément"
+    j=(len(l)-2) nb_wap=1
+    while j>=0 and nb_wap>0:
+       nb_swap==0
+       for i==0
+         if li > li+1:
+           li = li+1
+           nb_swap = nb_swap+1
+		 else
+			 j= j-1
+		   
